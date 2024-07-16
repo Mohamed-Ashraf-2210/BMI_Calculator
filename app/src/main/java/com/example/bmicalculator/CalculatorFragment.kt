5cebab84a12aea93fc17a44c19d4c71aed677f62
Mohamed Ashraf <ma1261327@gmail.com>
2024-07-17 02:57:10 +0300

    Finish CalculatorFragment

--- app/src/main/java/com/example/bmicalculator/CalculatorFragment.kt
+++ app/src/main/java/com/example/bmicalculator/CalculatorFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bmicalculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +11,60 @@ import com.example.bmicalculator.databinding.FragmentCalculatorBinding
 
 class CalculatorFragment : Fragment() {
 
-    private lateinit var binding: FragmentCalculatorBinding
+    private var binding: FragmentCalculatorBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentCalculatorBinding.inflate(inflater,container,false)
+    ): View? {
+        binding = FragmentCalculatorBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return binding.root
+        actionBtn()
+        return binding?.root
+    }
+
+    @SuppressLint("SetTextI18n", "ResourceAsColor")
+    private fun actionBtn() {
+        binding?.apply {
+            plusAge.setOnClickListener {
+                val age = ageNumber.text.toString().toInt()
+                if (age < 99)
+                    ageNumber.text = (age + 1).toString()
+            }
+
+            minusAge.setOnClickListener {
+                val age = ageNumber.text.toString().toInt()
+                if (age > 1)
+                    ageNumber.text = (age - 1).toString()
+            }
+
+            plusWeight.setOnClickListener {
+                weightNumber.text = (weightNumber.text.toString().toInt() + 1).toString()
+            }
+
+            minusWeight.setOnClickListener {
+                weightNumber.text = (weightNumber.text.toString().toInt() - 1).toString()
+            }
+
+            slider.addOnChangeListener { _, value, _ ->
+                heightNumber.text = value.toInt().toString()
+            }
+
+            maleCard.setOnClickListener {
+                maleSelect.visibility = View.VISIBLE
+                femaleSelect.visibility = View.GONE
+            }
+
+            femaleCard.setOnClickListener {
+                maleSelect.visibility = View.GONE
+                femaleSelect.visibility = View.VISIBLE
+            }
+
+            calculateBmiBtn.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.main, ResultFragment())
+                    .commit()
+            }
+        }
     }
 
 }
